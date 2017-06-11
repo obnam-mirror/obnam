9e1f796959cf377cb1bb6e23ab1949f29809f6c8
Lars Wirzenius <liw@liw.fi>
2017-06-11 21:06:48 +0300

    Fix: drop unnecessary, duplicate removal of ununused chunks

--- obnamlib/plugins/forget_plugin.py
+++ obnamlib/plugins/forget_plugin.py
@@ -103,7 +103,6 @@ class ForgetPlugin(obnamlib.ObnamPlugin):
         # Commit or unlock everything.
         self.repo.commit_client(client_name)
         self.repo.commit_chunk_indexes()
-        self.repo.remove_unused_chunks()
         self.repo.unlock_everything()
         self.app.dump_memory_profile('after committing')
 
